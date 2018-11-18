255c6d6a107362242fb2e6af9a1c3e4a52a8c54b
itiya <fragment.vertex@gmail.com>
2018-11-18 21:43:10 +0900

    [refactor] entry_pointの整頓

--- entry_point.rb
+++ entry_point.rb
@@ -4,23 +4,29 @@ require './course_maker/course_maker'
 app_dir = '/Applications/StepMania-5.0.12'
 
 
-setting_hash = {
-    target_level:10,
-    sample_number:8,
-    target_bpm_ranges:[[400, 430], [380, 400], [300, 380]],
-    course_name:'WarmingUp',
-    meter:'Medium:10',
-    scripter:'Thalathalaylah'
-}
+setting_hash_array = [
+    {
+        target_level: 10,
+        sample_number: 8,
+        target_bpm_ranges: [[400, 430], [380, 400], [300, 380]],
+        course_name: 'WarmingUp',
+        meter: 'Medium:10',
+        scripter: 'Thalathalaylah'
+    },
+    {
+        target_level: 11,
+        sample_number: 4,
+        target_bpm_ranges: [[400, 430], [380, 400], [300, 380]],
+        course_name: 'Advance',
+        meter: 'Medium:10',
+        scripter: 'Thalathalaylah'
+    }
+]
 
-setting = CourseMaker::CourseSetting.new(setting_hash)
-CourseMaker.make(app_dir, setting)
+setting_hash_array.each do |setting_hash|
+  setting = CourseMaker::CourseSetting.new(setting_hash)
+  CourseMaker.make(app_dir, setting)
+end
 
-setting_hash[:target_level] = 11
-setting_hash[:sample_number] = 4
-setting_hash[:course_name] = 'Advance'
-
-setting = CourseMaker::CourseSetting.new(setting_hash)
-CourseMaker.make(app_dir, setting)
 
 Open3.pipeline_rw('open ' + app_dir + '/StepMania.app')
