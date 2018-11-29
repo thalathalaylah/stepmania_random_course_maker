e766e7486ec97a8a5c619d27c162edda651397ee
itiya <fragment.vertex@gmail.com>
2018-11-29 23:28:13 +0900

    [add] 外部からコース設定を取得できる

---
+++ course_example.rb
@@ -0,0 +1,18 @@
+[
+    {
+        target_level: 10,
+        sample_number: 8,
+        target_bpm_ranges: [[400, 430], [380, 400], [300, 380]],
+        course_name: 'WarmingUp',
+        meter: 'Medium:10',
+        scripter: 'Thalathalaylah'
+    },
+    {
+        target_level: 11,
+        sample_number: 4,
+        target_bpm_ranges: [[400, 430], [380, 400], [300, 380]],
+        course_name: 'Advance',
+        meter: 'Medium:10',
+        scripter: 'Thalathalaylah'
+    }
+]
--- entry_point.rb
+++ entry_point.rb
@@ -2,28 +2,16 @@ require 'open3'
 require './course_maker/course_maker'
 require 'dotenv'
 
+
+if ARGV.size != 1
+  puts 'usage: entry_point.rb course_setting_file'
+  exit(-1)
+end
+
 Dotenv.load
 app_dir = ENV['STEPMANIA_APPLICATION_DIRECTORY']
 
-
-setting_hash_array = [
-    {
-        target_level: 10,
-        sample_number: 8,
-        target_bpm_ranges: [[400, 430], [380, 400], [300, 380]],
-        course_name: 'WarmingUp',
-        meter: 'Medium:10',
-        scripter: 'Thalathalaylah'
-    },
-    {
-        target_level: 11,
-        sample_number: 4,
-        target_bpm_ranges: [[400, 430], [380, 400], [300, 380]],
-        course_name: 'Advance',
-        meter: 'Medium:10',
-        scripter: 'Thalathalaylah'
-    }
-]
+setting_hash_array = eval File.read ARGV[0]
 
 setting_hash_array.each do |setting_hash|
   setting = CourseMaker::CourseSetting.new(setting_hash)
